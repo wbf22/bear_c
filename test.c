418476184d48a0661567cfd08a1c1998cdb6699a
wbf22 <brandon@fivefowlers.com>
2025-06-26 11:26:06 -0600

    Got make script working and started on list implementation

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    double b = 3.14;
+    char c = 'Z';
+    printf("Integer: %d\n", a);
+    printf("Double: %.2f\n", b); // 2 decimal places
+    printf("Character: %c\n", c);
+    printf("String: %s\n", "Hello");
+    return 0;
+}
